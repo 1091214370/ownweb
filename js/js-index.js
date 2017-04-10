5677886b9c3a2932fd664cfb6594055e4207f834
xch <1091214370@qq.com>
2017-04-10 20:33:10 +0800

    实现了简单的动画，基本完成了个人简历

--- js/js-index.js
+++ js/js-index.js
@@ -1,25 +1,36 @@
 window.onload=function(){
+	var box=document.getElementById('box');
 	var lis=document.getElementsByClassName('navli');
 	var lists=document.getElementsByClassName('list');
-	var icons=document.getElementsByClassName('iconfont')
+	box.style.transform="scale(0.1)";
+	setTimeout(function(){
+		box.style.transitionDelay="0";
+		box.style.transitionDuration="800ms";
+		box.style.transform="scale(1)";
+		box.style.opacity="0.9";
+	},500);
 	lis[0].style.background="#16d069";
-	lis[0].style.fontSize="20px";
-	icons[0].style.fontSize="35px";
 	lists[0].style.display="block";
 	for(var i=0;i<lis.length;i++){
 		lis[i].index=i;
 		lis[i].onclick=function(){
 			for(var s=0;s<lis.length;s++){
 				lis[s].style.background="";
-				lis[s].style.fontSize="";
-				icons[s].style.fontSize="";
 				lists[s].style.display="none";
 			}
-			var a=this.index;
+			var a=this.index;	
 			lis[a].style.background="#16d069";
-			lis[a].style.fontSize="20px";
-			icons[a].style.fontSize="35px";
 			lists[a].style.display="block";
-		}
+			lists[a].style.transform="scale(2)";
+			lists[a].style.opacity="0";
+			setTimeout(function(){
+				lis[a].style.background="#16d069";
+			lists[a].style.display="block";
+			lists[a].style.transitionDelay="0";
+			lists[a].style.transitionDuration="800ms";
+			lists[a].style.transform="scale(1)";
+			lists[a].style.opacity="1";
+			},500);
+			}
 	}
 }
